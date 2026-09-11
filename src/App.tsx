319d457acb92860db64cfd7d8a41b9f97bec4047
Pablo Buitrón <pablobuitron@192.168.1.55>
2026-09-11 13:36:30 +0200

    small changes added

--- src/App.tsx
+++ src/App.tsx
@@ -117,8 +117,8 @@ function App() {
                     key={item}
                     onClick={() => handleNavClick(item)}
                     className={`px-3 py-2 text-sm font-medium rounded-md transition-colors ${activeSection === item.toLowerCase().replace(/ /g, '-')
-                        ? 'bg-[#666666] text-[#17172F]'
-                        : 'text-[#17172F] hover:bg-gray-100 hover:text-[#F53A4B]'
+                        ? 'bg-[#666666] text-[#17172F]font-bold'
+                      : 'text-[#17172F] hover:bg-gray-100 hover:text-[#F53A4B] font-normal'
                       }`}
                   >
                     {item}
